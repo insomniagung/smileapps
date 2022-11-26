45b2feec312d33ed15fd98f85aff85783eaed27c
firmanxyz <mrfirman081@gmail.com>
2022-11-26 13:07:58 +0700

    Menambah Text Pada Home Dan Styling

--- src/views/pages/HomePage.js
+++ src/views/pages/HomePage.js
@@ -4,6 +4,8 @@ import bannerImage from "../../image/banner.jpg";
 function HomePages() {
   return (
     <div className="homepage-banner">
+      <h1 className="homepage-text">Smile Apps</h1>
+      <p className="">Harmonious Family Solution</p>
       <img src={bannerImage} alt="banner" />
     </div>
   );
