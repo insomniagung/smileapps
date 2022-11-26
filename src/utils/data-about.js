48681c5fe3071b717aaa8e0cead0d587b4ddc318
SadamMahendra <sadam.mahendra@gmail.com>
2022-11-26 22:58:24 +0700

    menambahkan icon serta link linkedin di about us

--- src/utils/data-about.js
+++ src/utils/data-about.js
@@ -4,24 +4,28 @@ let about = [
     name: "Agung Gunawan",
     collage: "darma persada",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui doloribus iure alias harum. Vitae necessitatibus laborum commodi ullam soluta vel, quidem praesentium, non exercitationem fugiat aliquid ipsa. Ullam, tenetur dolores.",
+    profile: "https://github.com/insomniagung",
   },
   {
     id: "about-2",
     name: "Firmansyah",
     collage: "darma persada",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui doloribus iure alias harum. Vitae necessitatibus laborum commodi ullam soluta vel, quidem praesentium, non exercitationem fugiat aliquid ipsa. Ullam, tenetur dolores.",
+    profile: "https://github.com/firmanxyz",
   },
   {
     id: "about-3",
     name: "Santi Septianti",
     collage: "jauh",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui doloribus iure alias harum. Vitae necessitatibus laborum commodi ullam soluta vel, quidem praesentium, non exercitationem fugiat aliquid ipsa. Ullam, tenetur dolores.",
+    profile: "https://github.com/Septiani272",
   },
   {
     id: "about-4",
     name: "Muhammad Sadam Mahendra",
     collage: "Darma Persada",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui doloribus iure alias harum. Vitae necessitatibus laborum commodi ullam soluta vel, quidem praesentium, non exercitationem fugiat aliquid ipsa. Ullam, tenetur dolores.",
+    profile: "https://github.com/SadamMahendra",
   },
 ];
 
