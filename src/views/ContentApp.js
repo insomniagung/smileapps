20d6cbf0fba331a6bcdf03ad3f09b17c7a46215b
SadamMahendra <sadam.mahendra@gmail.com>
2022-12-06 22:41:08 +0700

    menambahkan login

--- src/views/ContentApp.js
+++ src/views/ContentApp.js
@@ -3,8 +3,64 @@ import HomePages from "./pages/HomePage";
 import logoLove from "../image/logolove2.png";
 import ArticlePage from "./pages/ArticlePage";
 import AboutPage from "./pages/AboutPage";
+import LoginPage from "./pages/LoginPage";
+import { getUserLogged, putAccessToken } from "../utils/network-data";
+import { useEffect, useState } from "react";
 
 function ContentApp() {
+  const [authedUser, setAuthedUser] = useState(null);
+  const [initializing, setInitializing] = useState(true);
+
+  async function onLoginSuccess({ accessToken }) {
+    putAccessToken(accessToken);
+    const { data } = await getUserLogged();
+    setAuthedUser(data);
+  }
+
+  useEffect(() => {
+    async function fetchWithData() {
+      const { data } = await getUserLogged();
+      setAuthedUser(data);
+      setInitializing(false);
+    }
+
+    fetchWithData();
+  }, []);
+
+  function onLogout() {
+    setAuthedUser(null);
+    putAccessToken("");
+  }
+
+  if (initializing === true) {
+    return;
+  }
+
+  if (authedUser === null) {
+    return (
+      <div className="app-container">
+        <header className="app-bar">
+          <div className="app-bar__menu">
+            <button id="hamburgerButton">☰</button>
+          </div>
+          <div className="app-bar__brand">
+            <Link to="/" className="logo">
+              <img src={logoLove} alt="logo" className="logo__image" />
+              <h1>Sistem Informasi Parenting Style</h1>
+            </Link>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route
+              path="/*"
+              element={<LoginPage loginSuccess={onLoginSuccess} />}
+            />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
   return (
     <div className="app-container">
       <header className="app-bar">
@@ -43,6 +99,11 @@ function ContentApp() {
                 About
               </NavLink>
             </li>
+            <li>
+              <button className="button-logout" onClick={onLogout}>
+                logout ?
+              </button>
+            </li>
           </ul>
         </nav>
       </header>
@@ -51,6 +112,7 @@ function ContentApp() {
           <Route path="/" element={<HomePages />} />
           <Route path="/article" element={<ArticlePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/*" element={<LoginPage />} />
         </Routes>
       </main>
     </div>
